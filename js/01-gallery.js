32cc1a38685bf178b51e8c208f7689d6f76aa02f
Petro Bilous <bilouspm@gmail.com>
2023-03-04 01:58:49 +0200

    performs the second task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,25 +1,20 @@
 import { galleryItems } from './gallery-items.js';
-// console.log(galleryItems);
-
-// Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні.
-// Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
-
 
 const galleryContainer = document.querySelector('.gallery');
-const galleryMarup = createImgCardMarcup(galleryItems);
+const galleryMarkup = createImgCardMarcup(galleryItems);
 
-galleryContainer.innerHTML = galleryMarup;
+galleryContainer.innerHTML = galleryMarkup;
 
 function createImgCardMarcup(array) {
-    return array.map((item) => {
+    return array.map(({preview, original, description}) => {
         return`
         <div class="gallery__item">
           <a class="gallery__link" href="large-image.jpg">
             <img
               class="gallery__image"
-              src="${item.preview}"
-              data-source="${item.original}"
-              alt="${item.description}"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
             />
           </a>
         </div>`;
@@ -31,59 +26,24 @@ galleryContainer.addEventListener('click', hendleGalleryCardClick);
 
 function hendleGalleryCardClick(event) {
     event.preventDefault();
+  
+    document.addEventListener('keydown', hendleCloseGalleryCardKeydown);
     
-    document.addEventListener('keydown', off);
-    const isTargetPictureEl = event.target.classList.contains('gallery__image');
-
-    if (!isTargetPictureEl) {
-    return;
-    };
-
+    if (!event.target.classList.contains('gallery__image')) {
+      return;
+  };
+  
     const instance = basicLightbox.create(`
-	 <img src="${event.target.dataset.source}">`);
-
-    instance.show();
-
-    if (!instance.visible()) {
-        document.removeEventListener('keydown', off);
-    }
+    <img src="${event.target.dataset.source}">`);
     
-    function off() {
-
-        console.log(event);
-        if (event.coud === "Escape") {
-            instance.close()
-        }
+   instance.show();
     
-        
-    }
-    
-}
-
-
-
-
-// console.log(instance.visible())
-
-
-
-
-// Реалізація делегування на div.gallery і отримання url великого зображення.
-
-// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox.Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані(.min) файли бібліотеки.
-
-// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-
-// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
-
-// Розмітка елемента галереї
-// Посилання на оригінальне зображення повинно зберігатися в data-атрибуті source на елементі <img>, і вказуватися в href посилання. Не додавай інші HTML теги або CSS класи, крім тих, що містяться в цьому шаблоні.
-
-
-// Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку. Заборони цю поведінку за замовчуванням.
-
-// Закриття з клавіатури
-// УВАГА
-// Наступний функціонал не обов'язковий для здавання завдання, але буде хорошою додатковою практикою.
-
-// Додай закриття модального вікна після натискання клавіші Escape. Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно. Бібліотека basicLightbox містить метод для програмного закриття модального вікна.
+  function hendleCloseGalleryCardKeydown(event) {
+   
+    if (event.code === "Escape" && instance.visible() === true) {
+      
+      instance.close()
+      document.removeEventListener('keydown', hendleCloseGalleryCardKeydown);
+    };
+  };
+};
